c6b1871cf59a110c9ad0b1ea31ae42afc6273bcc
mok <99533961+parcPrive@users.noreply.github.com>
2024-09-13 18:37:07 +0900

    redis

--- demo/src/main/java/com/redis/demo/DemoApplication.java
+++ demo/src/main/java/com/redis/demo/DemoApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cache.annotation.EnableCaching;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
